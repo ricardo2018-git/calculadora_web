2794ba36e2baeaa926c24fec69615ee7e6d57474
ricardo2018-git <rp_mira@hotmail.com>
2018-07-13 23:25:15 -0300

    classe Calculadora

---
+++ classes/calculadora.php
@@ -0,0 +1,28 @@
+<?php
+
+	class Calculadora{
+		
+		private $total;
+		private $num1;
+		private $num2;
+		
+		function __construct(){
+			
+			$this -> total = 0;
+			$this -> num1  = 0;
+			$this -> num2  = 0;
+		}
+		
+		public function setNum1($parametro_num1){
+			
+			$this -> num1 = $parametro_num1;
+		}
+		
+		public function setNum2($parametro_num2){
+			
+			$this - num2 = $parametro_num2;
+		}
+		
+	}
+
+?>
