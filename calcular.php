ac55d8f2af1ea20363037ac1a25f74683863e7c4
ricardo2018-git <rp_mira@hotmail.com>
2018-07-13 23:34:56 -0300

    recebendo valor do usuario eoperação POST

--- calcular.php
+++ calcular.php
@@ -1,5 +1,15 @@
 <?php
 
+	require_once 'classe/calculadora.php';
 	
+	$num1     = $_POST['num1'];
+	$num2     = $_POST['num2'];
+	$operacao = $_POST['operacao'];
+	
+	$calculadora = new Calculadora();
+	
+	//Setar valores
+	$calculadora -> setNum1($num1);
+	$calculadora -> setNum2($num2);
 
 ?>
